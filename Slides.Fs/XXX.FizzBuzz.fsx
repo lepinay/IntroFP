0759f34383b5a86817db729869252e7217b7b1ef
Lepinay Laurent <lepinay@gmail.com>
2014-10-09 18:43:10 +0200

    reorg

--- Slides.Fs/XXX.FizzBuzz.fsx
+++ Slides.Fs/XXX.FizzBuzz.fsx
@@ -1,7 +1,8 @@
 ﻿for c in [1..100] do
    match c%3,c%5 with
-       | 0,0 -> printfn "FizzBuzz" 
-       | 0,_ -> printfn "Fizz"
-       | _,0 -> printfn "Buzz"
-       | _ -> printfn "%d" c
+       | 0,0 ->  "FizzBuzz" 
+       | 0,_ ->  "Fizz"
+       | _,0 ->  "Buzz"
+       | _ ->  c.ToString()
+    |> printfn "%A"
 
