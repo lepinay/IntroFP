0fff2d6e31df5e1cf79a62618c7629c358eb6a9a
Lepinay Laurent <lepinay@gmail.com>
2014-10-21 14:03:42 +0200

    v 0.2

---
+++ Slides.Cs/019.Maybe.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+class Maybe
+{
+    class PhoneNumber
+    {
+        public string Home { get; set; }
+        public string Work { get; set; }
+    }
+    class Detail
+    {
+        public PhoneNumber PhoneNumber { get; set; }
+
+        // More details
+    }
+    class Person
+    {
+        public Detail PersonalDetail { get; set; }
+    }
+
+    public static void Main()
+    {
+        var person = new Person();
+        string homeNumber;
+        if (person.PersonalDetail != null)
+        {
+            if (person.PersonalDetail.PhoneNumber != null)
+            {
+                if (person.PersonalDetail.PhoneNumber.Home != null)
+                {
+                    homeNumber = person.PersonalDetail.PhoneNumber.Home.ToUpper();
+                }
+                else homeNumber = "No home number";
+            }
+        }
+
+        //person?.PersonDetail?.PhoneNumber?.Home?.ToUpper();
+
+
+    }
+}
