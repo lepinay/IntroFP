4f45ecbf52625dd7598e78de3475113a3968377a
Lepinay Laurent <lepinay@gmail.com>
2014-10-23 15:47:12 +0200

    cleaning

--- SwapIt.Domain.Fs/Library1.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace SwapIt.Domain.Fs
-open Microsoft.Xna.Framework
-open Microsoft.Xna.Framework.Content
-open Microsoft.Xna.Framework.Graphics
-open Microsoft.Xna.Framework.Input
-open Microsoft.Xna.Framework.Storage
-open Microsoft.Xna.Framework.GamerServices
-
-
